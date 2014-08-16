ea3c9fcb20fbb63c8e4a9caeaa4ae680411057ab
Emil Daniel Ordoñez Avila <emil.ordonez@gmail.com>
2014-08-16 11:26:55 -0600

    Agregar variables para la cadena de conexion
    En lugar de escribir la cadena de conexion en la creacion del PDO es mejor crear variables porque yo estoy usando una BD SQL Server y Osly una MySQL

--- Pantallas_10Agosto/Registro.php
+++ Pantallas_10Agosto/Registro.php
@@ -6,11 +6,14 @@ class Registro {
     
     public static $PDO;
 	
+	$cadenaMysql='mysql:host=localhost;dbname=bdreportes;charset=utf8';
+	$cadenaMssql;
+	
 	function __construct() {
 		try{
 		    // Se envia en la cadena de conexion el charset=utf8, aunque ya la base de datos esté
 		    // configurada con el charset utf8 y el collation igual a utf8_unicode_ci
-            self::$PDO = new PDO('mysql:host=localhost;dbname=bdreportes;charset=utf8','root', '');
+            self::$PDO = new PDO($cadenaMysql,'root', '');
         }catch(Exception $e){
             throw new Exception("Error al conectarse");
         } 
