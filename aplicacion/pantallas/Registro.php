113e6b1963d40910c3b2003aabf98863b99ab3f3
emilordonez <emil.ordonez@gmail.com>
2014-08-20 16:22:29 -0600

    Cambios en el mapper de prueba login

--- aplicacion/pantallas/Registro.php
+++ aplicacion/pantallas/Registro.php
@@ -6,17 +6,20 @@ class Registro {
     
     public static $PDO;
 	
-	$cadenaMysql='mysql:host=localhost;dbname=bdreportes;charset=utf8';
-	$cadenaMssql="dblib:dbname=mpsiafireportes;host=192.168.1.10";
-	$usuario="sa";
-	$clave="emildaniel";
+	public static $errorDesc;
+	protected $cadenaMysql='mysql:host=localhost;dbname=bdreportes;charset=utf8';
+	protected $cadenaMssql="dblib:dbname=mpsiafireportes;host=192.168.1.10";
+	protected $usuario="sa";
+	protected $clave="emildaniel";
 	
 	function __construct() {
 		try{
 		    // Se envia en la cadena de conexion el charset=utf8, aunque ya la base de datos esté
 		    // configurada con el charset utf8 y el collation igual a utf8_unicode_ci
             self::$PDO = new PDO($cadenaMssql,$usuario, $clave);
+		$errorDesc = "Conexion Exitosa";
         }catch(Exception $e){
+		$errorDesc ="Error en la conexion: ".$e->getMessage();
             throw new Exception("Error al conectarse");
         } 
 	}
---
+++ aplicacion/pantallas/indice.php
@@ -0,0 +1,11 @@
+<h1>Titulo</h1>
+
+<?php
+echo "<h3>Hola Mundo</h3>";
+?>
+
+
+
+<p>
+	Parrafo
+</p>
